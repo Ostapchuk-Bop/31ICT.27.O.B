e99219466cb3fc25f2213c83a73ba05e155727db
bogda <bogdanostapcuk78@gmail.com>
2026-04-17 21:20:36 +0300

    add file (lab 3.1)

--- app/api/router.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from app.api.endpoints import users
-
-api_router = APIRouter()
-
-# Додаємо маршрут /users
-api_router.include_router(users.router, prefix="/users", tags=["users"])
